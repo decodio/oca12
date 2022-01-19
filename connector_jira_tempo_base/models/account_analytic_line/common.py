b283dab733b4dfb880bb9cb359d710865438b740
gkliska <goran@ecodica.eu>
2022-01-19 12:23:37 +0100

    revert

--- connector_jira_tempo_base/models/account_analytic_line/common.py
+++ connector_jira_tempo_base/models/account_analytic_line/common.py
@@ -18,7 +18,6 @@ class WorklogAdapter(Component):
 
     def read(self, issue_id, worklog_id):
         worklog = super().read(issue_id, worklog_id)
-        return worklog  # KGB temp
         if self.env.context.get('jira_worklog_no_tempo_timesheets_data'):
             return worklog
         with self.handle_404():
